03b4d5adf734ccbc7e0c7ffee2be6d5f0c69a436
holovchuchka <120129831+HolovchukNatalia@users.noreply.github.com>
2025-07-27 00:02:05 +0300

    Update usersRoutes.js

--- src/routers/usersRoutes.js
+++ src/routers/usersRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { getSavedArticles, saveArticleToUser } from '../controllers/articles';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { getSavedArticles, saveArticleToUser } from '../controllers/articles.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 router.post('/:userId/saved/:articleId', ctrlWrapper(saveArticleToUser));
