d6e619fbed33dbcac025e1190c41f5a521640cba
Eugen Alekseev <devilsbrother1@gmail.com>
2025-07-31 17:23:44 +0300

    add validate to save\delite article from user service

--- src/services/users/deleteArticleFromUserService.js
+++ src/services/users/deleteArticleFromUserService.js
@@ -4,7 +4,6 @@ import createHttpError from 'http-errors';
 
 export const deleteArticleFromUserService = async (userId, articleId) => {
   const user = await User.findById(userId);
-
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
@@ -14,14 +13,16 @@ export const deleteArticleFromUserService = async (userId, articleId) => {
   }
 
   const index = user.saved.indexOf(articleId);
-  if (index !== -1) {
-    user.saved.splice(index, 1);
-    await user.save({ validateBeforeSave: false });
+  if (index === -1) {
+    throw createHttpError(404, 'Article not found in saved');
+  }
 
-    const article = await Article.findById(articleId);
-    if (article && article.rate && article.rate > 0) {
-      article.rate -= 1;
-      await article.save({ validateBeforeSave: false });
-    }
+  user.saved.splice(index, 1);
+  await user.save({ validateBeforeSave: false });
+
+  const article = await Article.findById(articleId);
+  if (article && article.rate && article.rate > 0) {
+    article.rate -= 1;
+    await article.save({ validateBeforeSave: false });
   }
 };
