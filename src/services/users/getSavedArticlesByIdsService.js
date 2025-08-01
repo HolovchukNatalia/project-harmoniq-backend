997914f50579ccec7af785a4637281e112b05f7f
Eugen Alekseev <devilsbrother1@gmail.com>
2025-08-01 10:42:06 +0300

    1

--- src/services/users/getSavedArticlesByIdsService.js
+++ src/services/users/getSavedArticlesByIdsService.js
@@ -4,5 +4,13 @@ export const getSavedArticlesByIdsService = async (idsArray) => {
   if (!Array.isArray(idsArray) || idsArray.length === 0) {
     return [];
   }
-  return await Article.find({ _id: { $in: idsArray } });
+  const articles = await Article.find({ _id: { $in: idsArray } })
+    .populate('ownerId', 'name')
+    .lean();
+
+  return articles.map((article) => ({
+    ...article,
+    author: article.ownerId?.name || null,
+    ownerId: article.ownerId?._id || article.ownerId,
+  }));
 };
