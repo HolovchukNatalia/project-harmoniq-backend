aa88586febbbcfda83138fdbcda4472ba076c2bd
Eugen Alekseev <devilsbrother1@gmail.com>
2025-08-05 00:00:35 +0300

    del old files

--- src/services/users/getPopularUsers.js
+++
@@ -1,8 +0,0 @@
-import User from '../../db/models/user.js';
-
-export const getPopularUsers = async (limit = 5) => {
-  return await User.find()
-    .sort({ articlesAmount: -1 })
-    .limit(limit)
-    .select('-password -email -saved');
-};
